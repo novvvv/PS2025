975234e89bd8f6d8df1fa93ec5a27473ffef764b
doil <85823762+novvvv@users.noreply.github.com>
2025-08-16 08:36:29 +0900

    Create C - Monotonically Increasing.cpp

---
+++ AtCoder/LINE Verda Programming Contest/C - Monotonically Increasing.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    // 입출력 속도 향상
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int N, M;
+    std::cin >> N >> M;
+
+    // M개의 숫자 중 어떤 것을 고를지 표시할 배열(marker)
+    std::vector<int> marker(M);
+
+    for (int i = 0; i < N; ++i) {
+        marker[i] = 1;
+    }
+
+    do {
+        for (int i = 0; i < M; ++i) {
+            if (marker[i] == 1) {
+                std::cout << i + 1 << " ";
+            }
+        }
+        std::cout << "\n";
+    } while (std::prev_permutation(marker.begin(), marker.end()));
+
+    return 0;
+}
