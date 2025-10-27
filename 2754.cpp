3c28b8c20fda08a4b4e98cfcc6f0806405111f01
doil <85823762+novvvv@users.noreply.github.com>
2025-10-28 03:54:27 +0900

    Create 2754.cpp

---
+++ 2754.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string grade;
+    cin >> grade;
+    
+    if (grade == "A+") cout << "4.3" << endl;
+    else if (grade == "A0") cout << "4.0" << endl;
+    else if (grade == "A-") cout << "3.7" << endl;
+    else if (grade == "B+") cout << "3.3" << endl;
+    else if (grade == "B0") cout << "3.0" << endl;
+    else if (grade == "B-") cout << "2.7" << endl;
+    else if (grade == "C+") cout << "2.3" << endl;
+    else if (grade == "C0") cout << "2.0" << endl;
+    else if (grade == "C-") cout << "1.7" << endl;
+    else if (grade == "D+") cout << "1.3" << endl;
+    else if (grade == "D0") cout << "1.0" << endl;
+    else if (grade == "D-") cout << "0.7" << endl;
+    else cout << "0.0" << endl;
+     
+}
